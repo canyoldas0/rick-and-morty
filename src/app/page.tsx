f15c32aa2d8e4302338c21cb0c1c850b9c16d509
Can Yoldas <canyoldas1997@gmail.com>
2023-12-03 12:54:40 +0100

    adopting gql to next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,24 @@
 import Image from 'next/image';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from '@apollo/client';
+import { getClient } from './apiClient';
+import { gql } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: 'https://rickandmortyapi.com/graphql',
-  cache: new InMemoryCache(),
-});
-
-async function getData() {
-  client
-    .query({
-      query: gql`
-        query GetCharacters {
-          characters {
-            results {
-              id
-              name
-            }
-          }
-        }
-      `,
-    })
-    .then((result) => {
-      const chars = result.data.characters;
-      console.log(chars);
-    });
-}
+const CHARACTERS_QUERY = gql`
+  query GetCharacters {
+    characters {
+      results {
+        id
+        name
+      }
+    }
+  }
+`;
 
 export default async function Home() {
-  const data = await getData();
+  const { data } = await getClient().query({ query: CHARACTERS_QUERY });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {data.name}
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
